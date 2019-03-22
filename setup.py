abfe94c0382c4d3c85bf435ffb85e935cd8345d0
Rotondwatshipota1 <tshipotar@gmail.com>
2019-03-22 11:47:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='workproject',
+    name='mypackage',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='recurtion and sort funtions',
+    description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<Rotondwatshipota1>/<workproject>',
-    author='<Rotondwatshipota1>',
-    author_email='<tshipotar@gmail.com>'
+    url='https://github.com/<username>/<package-name>',
+    author='<Your Name>',
+    author_email='<Your Email>'
 )
